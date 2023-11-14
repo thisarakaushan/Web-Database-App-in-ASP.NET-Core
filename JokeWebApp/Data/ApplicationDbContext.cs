abd27c16dd6c6c73ca8fdd661adf442b82440286
thisarakaushan <thisarak9992@gmail.com>
2023-11-14 21:16:29 +0530

    Added JokeWebApp directory

---
+++ JokeWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokeWebApp.Models;
+
+namespace JokeWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JokeWebApp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ JokeWebApp/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace JokeWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+            
+        }
+    }
+}
